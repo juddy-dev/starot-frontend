7ceb2a609a8f58b8b4f33aa0243c001000ebc3bf
Juddy Reina Pardo <tech.diidoo@gmail.com>
2025-03-01 09:58:38 -0500

    se condigura el deploy y se agrega boton de iniciar sesión en el header

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: 'export', // Habilita la exportación estática
   distDir: 'dist',
   images: {
     unoptimized: true, // Deshabilita la optimización de imágenes (necesario para GitHub Pages)
   },
-  basePath: '/starot',
-  assetPrefix: '/starot/',
+  basePath: isProd ? '/starot' : '',
+  assetPrefix: isProd ? '/starot/' : '',
 };
 
 export default nextConfig;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,12 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-black">
-      <header className="p-4 bg-transparent">Starot</header>
+      <header>
+        Starot
+        <button className="bg-black border border-white hover:bg-indigo-600 hover:border-black text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300">
+          Iniciar sesión
+        </button>
+      </header>
       <main className="p-0 m-0">{children}</main>
       <footer className="p-4 bg-transparent text-white text-center">© 2025 Starot</footer>
     </div>
